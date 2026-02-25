3c20962cb113af0bab2fad11ed30eb2c49d60ea6
Gregor Guerrier <gregorguerrier@dhcp-vl2041-17362.eduroam.cornell.edu>
2026-02-25 18:16:01 -0500

    Implemented Initial Navigation

---
+++ app/src/main/java/com/cornellappdev/chimes/ui/navigation/NavigationItem.kt
@@ -0,0 +1,26 @@
+package com.cornellappdev.chimes.ui.navigation
+
+sealed class NavigationItem(
+    val route: String,
+) {
+    object Home : NavigationItem (
+        route = Routes.HOME.name,
+    )
+}
+
+/**
+ * All NavUnit must have a route (which specifies where to
+ * navigate to).
+ */
+interface NavUnit {
+    val route: String
+}
+
+/**
+ * Contains information about all known routes. These should correspond to routes in our
+ * NavHost/new routes should be added here. Routes can exist independent of tabs (like onboarding).
+ */
+enum class Routes(override var route: String) : NavUnit {
+    HOME("home"),
+    ONBOARDING("onboarding"),
+}
