d4e8da2cfbf85d7a1064acb5f60ce061d8ef9e20
Your Name <firo618@gmail.com>
2026-03-26 01:04:03 -0400

    finished onboarding screen ui
    added login interface buttons and fixed all spacing requirements

--- app/src/main/java/com/cornellappdev/chimes/ui/screens/Onboarding.kt
+++ app/src/main/java/com/cornellappdev/chimes/ui/screens/Onboarding.kt
@@ -7,9 +7,12 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -29,6 +32,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cornellappdev.chimes.R
@@ -46,6 +50,7 @@ fun Onboarding () {
     val chimesAlpha = remember { Animatable(0f) }
     val headerOffsetY = remember { Animatable(0f) }
     val loginAlpha = remember { Animatable(0f) }
+    val loginOffsetY = remember { Animatable(300f) } // Starts 300dp below
     val minuteHandRotation = remember { Animatable(0f) }
     val hourHandRotation = remember { Animatable(0f) }
     var handsSpinStarted by remember { mutableStateOf(false) }
@@ -75,10 +80,13 @@ fun Onboarding () {
 
         delay(300)
         launch {
-            headerOffsetY.animateTo(-90f, tween(300, easing = FastOutSlowInEasing))
+            headerOffsetY.animateTo(-100f, tween(300, easing = FastOutSlowInEasing))
         }
         delay(300)
-        loginAlpha.animateTo(1f, tween(300, easing = LinearEasing))
+        launch {
+            loginOffsetY.animateTo(0f, tween(300, easing = FastOutSlowInEasing)) //slide
+        }
+        loginAlpha.animateTo(1f, tween(300, easing = LinearEasing)) //fade
     }
 
     LaunchedEffect(handsSpinStarted) {
@@ -102,7 +110,7 @@ fun Onboarding () {
         }
     }
 
-    Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
+    Box(modifier = Modifier.fillMaxSize().background(Color(0xFFFEF7F7))) {
 
         Image(
             painter = painterResource(id = R.drawable.ic_onboarding_background),
@@ -113,53 +121,19 @@ fun Onboarding () {
             contentScale = ContentScale.Crop
         )
 
-
-
-        /*
-        //with gradient fade
-        Image(
-            painter = painterResource(id = R.drawable.ic_onboarding_background),
-            contentDescription = "Onboarding background",
-            modifier = Modifier
-                .fillMaxSize()
-                .graphicsLayer{
-                    compositingStrategy = CompositingStrategy.Offscreen
-                }
-                .drawWithContent{
-                    drawContent()
-                    val brush = Brush.verticalGradient(
-                        0f to Color.White,
-                        bgAlpha.value to Color.Transparent
-                    )
-                    drawRect(brush = brush, blendMode = BlendMode.DstIn)
-                },
-            contentScale = ContentScale.Crop
-        )
-
-         */
-
-        /*
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .alpha(whiteAlpha.value)
-        )
-
-         */
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .padding(horizontal = 24.dp)
+                .offset(y = headerOffsetY.value.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(headerOffsetY.value.dp))
+
+            Spacer(modifier = Modifier.weight(1f)) //top half of screen
 
             ClockLogo(
                 modifier = Modifier
-                    .size((300 * logoScale.value).dp)
+                    .size((150 * logoScale.value).dp)
                     .alpha(logoAlpha.value),
                 minuteHandRotation = minuteHandRotation.value,
                 hourHandRotation = hourHandRotation.value
@@ -171,12 +145,140 @@ fun Onboarding () {
                 text = "chimes",
                 style = MaterialTheme.typography.titleMedium.copy(
                     fontWeight = FontWeight.Medium,
-                    color = Color(0xFFE24B4B)
+                    color = Color(0xFFCC5555)
                 ),
                 modifier = Modifier.alpha(chimesAlpha.value),
-                fontSize = 18.sp
+                fontSize = 40.sp
             )
-            Spacer(modifier = Modifier.height(140.dp))
+
+            // this box takes up the bottom half of the screen, so the login buttons won't push the logo up
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .alpha(loginAlpha.value)
+                        .offset(y = loginOffsetY.value.dp)
+                        .width(333.dp)
+                ) {
+                    val curve = 50.dp
+
+                    Spacer(modifier = Modifier.height(64.dp))
+
+                    Button(
+                        modifier = Modifier
+                            .height(54.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color(0XFFBCB2B2),
+                                shape = RoundedCornerShape(curve)
+                            )
+                            .fillMaxWidth(),
+                        onClick = {},
+                        shape = RoundedCornerShape(curve),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black
+                        ),
+                        contentPadding = PaddingValues(horizontal = 20.dp)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_google_g),
+                                contentDescription = "Google logo",
+                                modifier = Modifier.size(32.dp),
+                                contentScale = ContentScale.Fit
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(
+                                text = "Log-in with Google",
+                                style = MaterialTheme.typography.titleMedium.copy(
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color.Black
+                                ),
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Button(
+                        modifier = Modifier
+                            .height(54.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color(0XFFBCB2B2),
+                                shape = RoundedCornerShape(curve)
+                            )
+                            .fillMaxWidth(),
+                        onClick = {},
+                        shape = RoundedCornerShape(curve),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black
+                        ),
+                        contentPadding = PaddingValues(horizontal = 20.dp)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_cornell_logo),
+                                contentDescription = "Cornell logo",
+                                modifier = Modifier.size(32.dp),
+                                contentScale = ContentScale.Fit
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(
+                                text = "Log-in with Cornell netID",
+                                style = MaterialTheme.typography.titleMedium.copy(
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color.Black
+                                ),
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(45.dp))
+
+                    HorizontalDivider(
+                        color = Color(0XFFBCB2B2),
+                        thickness = 1.dp,
+                        modifier = Modifier
+                            .width(200.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(45.dp))
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                        ),
+                        shape = RoundedCornerShape(0.dp),
+                        modifier = Modifier.height(25.dp),
+                        contentPadding = PaddingValues(0.dp)
+                    ) {
+                        Text(
+                            text = "log in without an account",
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                fontWeight = FontWeight.Thin,
+                                color = Color.Black,
+                                textDecoration = TextDecoration.Underline
+                            ),
+                            fontSize = 8.sp
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -192,12 +294,12 @@ private fun ClockLogo(
         modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
-        val clockSize = maxWidth * (69f / 118f)
-        // Adjust these coefficients to make the hands smaller/larger
-        val minuteHandWidth = maxWidth * 0.21f
-        val minuteHandHeight = maxWidth * 0.12f
-        val hourHandWidth = maxWidth * 0.17f
-        val hourHandHeight = maxWidth * 0.14f
+        val clockSize = maxWidth
+        
+        val minuteHandWidth = maxWidth * 0.36f
+        val minuteHandHeight = maxWidth * 0.21f
+        val hourHandWidth = maxWidth * 0.29f
+        val hourHandHeight = maxWidth * 0.24f
 
         Image(
             painter = painterResource(id = R.drawable.ic_clock),
@@ -213,7 +315,8 @@ private fun ClockLogo(
             modifier = Modifier
                 .size(hourHandWidth, hourHandHeight)
                 .graphicsLayer {
-                    var value = 11f / 30f
+                    val value = 11f / 30f
+
                     translationX = (0.5f - value) * size.width
                     translationY = (0.5f - value) * size.height
                     
@@ -229,8 +332,8 @@ private fun ClockLogo(
             modifier = Modifier
                 .size(minuteHandWidth, minuteHandHeight)
                 .graphicsLayer {
-                    var xValue = 2.5f/25f
-                    var yValue = 16.5f/25f
+                    val xValue = 2.5f/25f
+                    val yValue = 16.5f/25f
 
                     translationX = (0.5f - xValue) * size.width
                     translationY = (0.5f - yValue) * size.height
